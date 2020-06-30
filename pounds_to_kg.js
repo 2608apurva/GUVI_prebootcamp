bcac5973ab53326f29e5566ebe09a5bae6492ad4
2608apurva <67631662+2608apurva@users.noreply.github.com>
2020-06-30 18:53:05 +0530

    Create pounds_to_kg.js

---
+++ pounds_to_kg.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+ userInput.push(data);
+});
+inp.on("close", () => {
+    
+var pounds= +userInput[0];
+var kg= pounds/2.205;
+
+console.log(kg);
+
+});
