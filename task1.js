759491a77263c52709f2f600fc77b6fc97cdf2a4
2608apurva <67631662+2608apurva@users.noreply.github.com>
2020-06-30 16:54:57 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,93 @@
+//Declare four variables without assigning values
+
+var a, b, c, d;
+
+console.log(a, b, c, d);
+
+//to get value of the variable myvar as output
+
+var myvar= 1;
+console.log("myvar");
+
+//variables to store your first name, last name, marital status, country and age in multiple lines
+
+var firstName;
+var lastName;
+var maritalStatus;
+var country;
+var age;
+
+//Declare variables to store your first name, last name, marital status, country and age in a single line
+
+var firstName, lastName, maritalStatus, country, age;
+
+//Declare variables and assign string, boolean, undefined and null data types
+
+var str= 'The name is john miller.';
+var istrue = true;
+var c;
+var n =" ";
+
+//Convert the string to integer
+
+var num = "10";
+console.log(parseInt(num));
+console.log(number(num));
+console.log(+(num));
+
+//6 statement which provide truthy & falsey values.
+
+var x=" ";//thus falsy value
+var b;//thus falsy value
+var c=0;//thus falsy value
+var e=parseInt("hello");//thus falsy value
+var f=10;
+var sen= 'Hello !!';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
