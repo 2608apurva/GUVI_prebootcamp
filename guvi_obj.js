274a38bf664ccc02f689d1a822f32691e4b9a151
2608apurva <67631662+2608apurva@users.noreply.github.com>
2020-07-07 01:31:06 +0530

    Create guvi_obj.js

---
+++ guvi_obj.js
@@ -0,0 +1,9 @@
+var guvi={
+    firstName:'Guvi',
+    lastName: 'Geek',
+    lane:6,
+    address1:'IIT-M RP',
+    address2:'Chennai'
+}
+
+console.log(guvi.firstName+','+guvi.lastName+','+guvi.lane+','+guvi.address1+','+guvi.address2);
