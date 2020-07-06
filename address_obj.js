073b5eebb07e72e0286cd4aff8215d3c65cb5577
2608apurva <67631662+2608apurva@users.noreply.github.com>
2020-07-07 01:48:31 +0530

    Update and rename guvi_obj.js to address_obj.js

---
+++ address_obj.js
@@ -0,0 +1,45 @@
+var guvi={
+    firstName:'Guvi',
+    lastName: 'Geek',
+    lane:6,
+    address1:'IIT-M RP',
+    address2:'Chennai'
+}
+
+console.log(guvi.firstName+','+guvi.lastName+','+guvi.lane+','+guvi.address1+','+guvi.address2);
+
+var amazon={
+    firstName:'Amazon',
+    lastName: 'Inc',
+    lane:31,
+    address1:'SP Infocity',
+    address2:'Chennai'
+
+}
+
+console.log(amazon.firstName+','+amazon.lastName+','+amazon.lane+','+amazon.address1+','+amazon.address2);
+
+var google={
+    firstName:'Google',
+    lastName: 'Alphabet',
+    lane:34,
+    address1:'Amphitheater Parkway',
+    address2:'Mountain View'
+}
+
+console.log(google.firstName+','+google.lastName+','+google.lane+','+google.address1+','+google.address2);
+
+var tesla={
+    firstName:'Tesla',
+    lastName: 'Inc',
+    lane:32,
+    address1:'SP 333 Santana Row',
+    address2:'San Jose'
+
+}
+
+console.log(tesla.firstName+','+tesla.lastName+','+tesla.lane+','+tesla.address1+','+tesla.address2);
+
+
+
+
--- guvi_obj.js
+++
@@ -1,9 +0,0 @@
-var guvi={
-    firstName:'Guvi',
-    lastName: 'Geek',
-    lane:6,
-    address1:'IIT-M RP',
-    address2:'Chennai'
-}
-
-console.log(guvi.firstName+','+guvi.lastName+','+guvi.lane+','+guvi.address1+','+guvi.address2);
